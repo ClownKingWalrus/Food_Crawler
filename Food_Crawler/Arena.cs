16ff0ab3341699902fe87ff5198289ba94444296
Walrus <buttermunchinwalrus@gmail.com>
2025-03-22 01:39:14 -0400

    going to pull player so i can work with the player object

---
+++ Food_Crawler/Arena.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//the purpose of this class is to handle all combat interactions
+//feel free to add some unique combat features as we will be adding random effects over time
+//remeber RNG is the king of fighters as long as its managable
+namespace Food_Crawler
+{
+    class Arena
+    {
+        private static void NormalFight()
+        {
+
+        }
+    }
+}
